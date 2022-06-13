47d58e9f46a2c12859de1e3dbb251b5e74c60e20
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-06-14 00:56:56 +1000

    refactoring login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,10 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from "@apollo/react-hooks";
 
-import { loginUser } from '../utils/API';
+//import { loginUser } from '../utils/API';
+import { LOGIN_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -26,15 +28,19 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      // const response = await loginUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // const { token, user } = await response.json();
+      // console.log(user);
+      // Auth.login(token);
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
